37643298eb3dc30f23020a489b7fedc9c97390cb
tntyousif <yousif02bh@gmail.com>
2024-11-13 13:16:29 +0300

    templet ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-// src/App.jsx
+// App.jsx
 
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+import SignupForm from './components/SignupForm/SignupForm' 
+import SigninForm from './components/SigninForm/SigninForm'
+
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   return (
     <>
       <NavBar user={user} />
-      <h1>Hello world!</h1>
-    </>
-  )
-}
+      <Routes>
+      {
+        user ?
+          <Route path="/" element={<Dashboard user={user} />} />
+        :
+          <Route path="/" element={<Landing />} />
+      }
 
-export default App
+        <Route path='/signup' element={<SignupForm setUser={setUser} />} />
+        <Route path='/signin' element={<SigninForm setUser={setUser} />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/services/authService.js
@@ -0,0 +1,47 @@
+// src/services/authService.js
+
+const BACKEND_URL = 'http://localhost:3000'; // this is our Express API url
+
+const signup = async (formData) => {
+  try {
+    const res = await fetch(`${BACKEND_URL}/users/signup`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    });
+    const json = await res.json();
+    if (json.err) {
+      throw new Error(json.err);
+    }
+    return json;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+
+const signin = async (user) => {
+  try {
+    const res = await fetch(`${BACKEND_URL}/users/signin`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    });
+    const json = await res.json();
+
+    if (json.error) {
+      localStorage.setItem('token', json.token); // add this line to store the JWT token in localStorage
+    }
+
+    if (json.token) {
+      const user = JSON.parse(atob(json.token.split('.')[1]));
+      return user;
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export { signup, signin };
